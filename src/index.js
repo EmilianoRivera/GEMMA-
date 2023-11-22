1e01c594b881b397ba40c541d34a700cf0be1fbb
Emiliano Rivera <jriveras2100@alumno.ipn.mx>
2023-11-22 02:55:39 -0600

    IMPLEMENTADO LA FUNCION DE DESPLEGAR LOS REPORTES Y AÑADIR EL CAMPO DE ESTADO

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import ejs from 'ejs'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+
 
 import indexRoutes from  './routes/index.js'
 
@@ -10,15 +14,24 @@ const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure express-session middleware
+app.use(session({
+  secret: 'your-secret-key',
+  resave: true,
+  saveUninitialized: true
+}));
 //CONFIGURACION PARA UNIR LA RUTA ABSOLUTA CON LA CARPETA DE VIEWS
 app.set('views', join(__dirname, 'views') )
 //MOTOR DE RENDERIZADO DE PLANTILLAS
 app.set('view engine', 'ejs')
 app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
-app.listen(3000)
-
-
-console.log("Server is listening",3000)
+//INICIADO DEL SERVIDOR
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,69 @@
-import { Router } from "express"; //me ayuda a definir distintas rutas
+// routes/index.js
+import { Router } from "express";
+import "dotenv/config";
+import { config } from "dotenv";
+
+config();
+
 const router = Router();
 
-router.get("/", (req, res) => res.render("index", {title: 'BACHECITO 26'
-}));
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+  if (req.session.user) {
+    // If the user is authenticated, proceed to the next middleware
+    next();
+  } else {
+    // If not authenticated, redirect to the login page
+    res.redirect("/error");
+  }
+};
 
-router.get("/loginMeta", (req, res) => res.render("loginMeta", {title : 'login'}));
+router.get("/", (req, res) =>
+  res.render("index", { title: "BACHECITO 26", user: req.session.user })
+);
 
-router.get("/peticiones", (req, res) => res.render("peticiones", {title: 'peticiones'}));
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === process.env.USUARIO && password === process.env.CONTRASENA) {
+    req.session.user = username;
+    res.redirect("/");
+  } else {
+    res.redirect("/inicioSesion");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+// Use the isAuthenticated middleware for routes that require authentication
+router.get("/peticiones", isAuthenticated, (req, res) =>
+  res.render("peticiones", { title: "Peticiones", user: req.session.user })
+);
+
+router.get("/sobreNosotros", (req, res) =>
+  res.render("sobreNosotros", {
+    title: "Sobre Nosotros",
+    user: req.session.user,
+  })
+);
+
+router.get("/inicioSesion", (req, res) =>
+  res.render("inicioSesion", {
+    title: "Inicio de Sesión",
+    user: req.session.user,
+  })
+);
+
+router.get("/bachecito", (req, res) =>
+  res.render("bachecito", { title: "Bachecito 26", user: req.session.user })
+);
+
+router.get("/error", (req, res) =>
+  res.render("error", { title: "ERROR", user: req.session.user })
+);
 
 export default router;
